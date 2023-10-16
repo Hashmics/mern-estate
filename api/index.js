107b759681f726f628f2c02a37b0a993fbcb5e63
Hashmics <rehmanhashmirehman123@gmail.com>
2023-10-16 23:18:18 +0500

    create signup Api

--- api/index.js
+++ api/index.js
@@ -1,7 +1,8 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv'
-import userRouter from './routes/user.routes.js'
+import userRouter from './routes/user.routes.js' 
+import authRouter from './routes/auth.routes.js'
 dotenv.config();
 
 mongoose.connect(process.env.MONGO).then(()=>{
@@ -10,11 +11,14 @@ mongoose.connect(process.env.MONGO).then(()=>{
         console.log(err); 
     })
 
-const app=express();
+const app=express(); 
+
+app.use(express.json());
 
 app.listen(3000,()=>{
     console.log('Server listening on port 3000');
     }
 );
 
-app.use("/api/user", userRouter)
+app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
